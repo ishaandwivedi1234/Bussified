2bc1264c36a5817036cea271ea67dd068bd0df61
ishaandwivedi1234 <ishaan.dwivedi99@gmail.com>
2020-12-08 19:31:01 +0530

    db added

--- yatram/settings.py
+++ yatram/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 import os as os
 
@@ -85,7 +86,8 @@ DATABASES = {
         'HOST': 'localhost'
     }
 }
-
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
